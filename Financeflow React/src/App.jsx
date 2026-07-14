1ba5985e37ddc9f87db11a3d623c878bf5770e12
madhumitha <madhumitha0286@gmail.com>
2026-07-14 12:59:41 +0530

    create Navbar component

--- Financeflow React/src/App.jsx
+++ Financeflow React/src/App.jsx
@@ -1,8 +1,8 @@
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <h1>FinanceFlow</h1>
-      <p>My React Journey Starts Today 🚀</p>
+      <Navbar />
     </div>
   );
 }
---
+++ Financeflow React/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>FinanceFlow</h1>
+      <a href="index.html">Dashboard</a>
+      <a href="transactions.html">Transactions</a>
+    </nav>
+  );
+}
+
+export default Navbar;
