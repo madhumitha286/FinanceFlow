a5f639a6b5a518d9cc83ca6f575886efe3112bc2
madhumitha <madhumitha0286@gmail.com>
2026-07-15 13:07:58 +0530

    router setup

--- Financeflow React/src/App.jsx
+++ Financeflow React/src/App.jsx
@@ -1,9 +1,24 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
